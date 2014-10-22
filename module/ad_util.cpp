d4f28eb2e9f01c3a9a3625dd5218036cf1c19eba
hiroshi Matoba <matobahiroshi@gmail.com>
2014-10-22 15:25:43 +0200

    add ad_util

---
+++ module/ad_util.cpp
@@ -0,0 +1,11 @@
+//
+//  ad_util.cpp
+//  crater_1
+//
+//  Created by hiroshi matoba on 10/22/14.
+//
+//
+
+#include "ad_util.h"
+
+string ad_util::data_path = "../../../../data/";
---
+++ module/ad_util.h
@@ -0,0 +1,19 @@
+//
+//  ad_util.h
+//  crater_1
+//
+//  Created by hiroshi matoba on 10/22/14.
+//
+//
+
+#pragma once
+
+#include "ofMain.h"
+
+class ad_util{
+    
+public:
+    
+    static string data_path;
+};
+

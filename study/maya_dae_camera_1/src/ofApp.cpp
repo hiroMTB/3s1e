d497fc6444ad6ba23400ea375b1bbe1d6b0d4f97
hiroMTB <matobahiroshi@gmail.com>
2014-12-18 13:53:04 +0100

    maya dae cam

--- study/maya_dae_camera_1/src/ofApp.cpp
+++ study/maya_dae_camera_1/src/ofApp.cpp
@@ -8,11 +8,7 @@ void ofApp::setup(){
     ofBackground(50, 0);
 
     ofDisableArbTex(); // we need GL_TEXTURE_2D for our models coords.
-
-    bAnimate = false;
-    bAnimateMouse = false;
-    animationPosition = 0;
-    
+	
     model.loadModel("B4.dae", true);
     const aiScene * scene = model.getAssimpScene();
     int n = scene->mNumCameras;
@@ -41,6 +37,11 @@ void ofApp::setup(){
             ss << quat << "\n";
             ss << rot << "\n";
             cout << ss.str() << endl;
+			
+			ofMatrix4x4 mat;
+			mat.translate( pos );
+			mat.rotate( quat );
+			cam.setTransformMatrix( mat );
         }
 
         
@@ -69,21 +70,11 @@ void ofApp::setup(){
     model.setPosition(ofGetWidth() * 0.5, (float)ofGetHeight() * 0.75 , 0);
     model.setLoopStateForAllAnimations(OF_LOOP_NORMAL);
     model.playAllAnimations();
-    if(!bAnimate) {
-        model.setPausedForAllAnimations(true);
-    }
-    
-    
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
     model.update();
-    
-    if(bAnimateMouse) {
-        model.setPositionForAllAnimations(animationPosition);
-    }
-
     mesh = model.getCurrentAnimatedMesh(0);
 }
 
@@ -132,94 +123,7 @@ void ofApp::draw(){
 void ofApp::keyPressed(int key){
     ofPoint modelPosition(ofGetWidth() * 0.5, (float)ofGetHeight() * 0.75);
     switch (key) {
-        case '1':
-            model.loadModel("astroBoy_walk.dae");
-            model.setPosition(modelPosition.x, modelPosition.y, modelPosition.z);
-            ofEnableSeparateSpecularLight();
-            break;
-        case '2':
-            model.loadModel("TurbochiFromXSI.dae");
-            model.setPosition(modelPosition.x, modelPosition.y, modelPosition.z);
-            model.setRotation(0, -180, 1, 0, 0);
-            ofEnableSeparateSpecularLight();
-            break;
-        case '3':
-            model.loadModel("dwarf.x");
-            model.setPosition(modelPosition.x, modelPosition.y, modelPosition.z);
-            ofDisableSeparateSpecularLight();
-            break;
-        case '4':
-            model.loadModel("monster-animated-character-X.X");
-            model.setPosition(modelPosition.x, modelPosition.y, modelPosition.z);
-            model.setRotation(0, -90, 0, 0, 1);
-            ofDisableSeparateSpecularLight();
-            break;
-		case '5':
-			model.loadModel("squirrel/NewSquirrel.3ds");
-		    model.setPosition(modelPosition.x, modelPosition.y, modelPosition.z);
-            ofDisableSeparateSpecularLight();
-			break;
-		case ' ':
-			bAnimate = !bAnimate;
-			break;
         default:
             break;
     }
-
-	mesh = model.getMesh(0);
-
-    model.setLoopStateForAllAnimations(OF_LOOP_NORMAL);
-    model.playAllAnimations();
-    if(!bAnimate) {
-        model.setPausedForAllAnimations(true);
-    }
 }
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-    //
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-    // scrub through aninations manually.
-	animationPosition = y / (float)ofGetHeight();
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-    // pause all animations, so we can scrub through them manually.
-    model.setPausedForAllAnimations(true);
-	animationPosition = y / (float)ofGetHeight();
-    bAnimateMouse = true;
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-    // unpause animations when finished scrubbing.
-    if(bAnimate) {
-        model.setPausedForAllAnimations(false);
-    }
-    bAnimateMouse = false;
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){
-
-}
-
--- study/maya_dae_camera_1/src/ofApp.h
+++ study/maya_dae_camera_1/src/ofApp.h
@@ -13,21 +13,9 @@ class ofApp : public ofBaseApp{
 		void draw();
 		
 		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);		
-    
+	
         ofxAssimpModelLoader model;
-    
-		bool bAnimate;
-        bool bAnimateMouse;
-        float animationPosition;
-
+	
         ofMesh mesh;
         ofLight	light;
     
